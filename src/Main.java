1b8fc75757f8dd3b966738acdf33fa954c9b35db
zyskkt <122432583+zyskkt@users.noreply.github.com>
2023-02-08 20:27:31 -0600

    Update Main.java

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,14 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+
+        //Initialize variables
+        int myChildren = 1;
+        boolean isItRaining = false;
+        double priceOfGas = 3.08;
+        int favNumber = 17;
+        double shoeSize = 10.5;
+        String birthMonth = "January";
+        String fullName = "Kent Zysk";
+
     }
 }
